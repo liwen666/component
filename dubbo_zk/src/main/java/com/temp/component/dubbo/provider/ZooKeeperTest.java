b3e5c721b15d76dcc11cf134685146d151e57af5
custom <1316138287@qq.com>
2019-03-14 17:48:45 +0800

    zookeeper完整测试

--- dubbo_zk/src/main/java/com/temp/component/dubbo/provider/ZooKeeperTest.java
+++ dubbo_zk/src/main/java/com/temp/component/dubbo/provider/ZooKeeperTest.java
@@ -1,26 +1,32 @@
 package com.temp.component.dubbo.provider;
  
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
  
 public class ZooKeeperTest {
  
     public static void main(String[] args) throws Exception{
-        ZooKeeper zk = new ZooKeeper("192.168.42.136:2181", 3000, null);
+        ZooKeeper zk = new ZooKeeper("192.168.42.220:2181", 3000, new Watcher() {
+            public void process(WatchedEvent event) {
+                // TODO Auto-generated method stub
+                System.out.println(" receive event : " + event.getType().name());
+            }
+        });
         System.out.println("=========创建节点===========");
-        if( null==zk.exists("/test", false))
-        {
-        	zk.create("/test", "znode1".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        if (null == zk.exists("/test", false)) {
+            zk.create("/test", "znode1".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         }
         System.out.println("=============查看节点是否安装成功===============");
         System.out.println(new String(zk.getData("/test", false, null)));
-        
+
         System.out.println("=========修改节点的数据==========");
         zk.setData("/test", "zNode2".getBytes(), -1);
         System.out.println("========查看修改的节点是否成功=========");
         System.out.println(new String(zk.getData("/test", false, null)));
-        
+
         System.out.println("=======删除节点==========");
         zk.delete("/test", -1);
         System.out.println("==========查看节点是否被删除============");
