a1cf577a968886e25b79f07ca5aa3f4a1e760eaa
custom <1316138287@qq.com>
2019-05-09 17:51:47 +0800

    sql注入检测

--- common/src/main/java/com/temp/common/common/regx/RegexExample1.java
+++ common/src/main/java/com/temp/common/common/regx/RegexExample1.java
@@ -1,8 +1,4 @@
-package com.temp.common.base.util;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+package com.temp.common.common.regx;
 /**
  * 正则表达式语法
 
@@ -217,103 +213,17 @@ import java.util.regex.Pattern;
 
  匹配 n，其中 n 是以四位十六进制数表示的 Unicode 字符。例如，\u00A9 匹配版权符号 (©)。
  */
-public class RegexMatches
-{
-    public static void main( String args[] ){
+
+import java.util.regex.Pattern;
+
+class RegexExample1{
+   public static void main(String args[]){
+      String content = "I am noob " +
+        "from runoob.com.";
  
-      // 按指定模式在字符串查找
-      String line = "This order was placed for 44QT3000! OK?33";
-      String pattern = "(\\D*)(\\d+)(.*)";
+      String pattern = ".*runoob.*";
  
-      // 创建 Pattern 对象
-      Pattern r = Pattern.compile(pattern);
- 
-      // 现在创建 matcher 对象
-      Matcher m = r.matcher(line);
-      if (m.find( )) {
-         System.out.println("Found value: " + m.group(0) );
-         System.out.println("Found value: " + m.group(1) );
-         System.out.println("Found value: " + m.group(2) );
-         System.out.println("Found value: " + m.group(3) ); 
-      } else {
-         System.out.println("NO MATCH");
-      }
+      boolean isMatch = Pattern.matches(pattern, content);
+      System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
    }
-}
-class RegexMatches1
-{
-    private static final String REGEX = "\\bcat\\b";
-    private static final String INPUT =
-            "cat cat cat cattie cat";
-
-    public static void main( String args[] ){
-        Pattern p = Pattern.compile(REGEX);
-        Matcher m = p.matcher(INPUT); // 获取 matcher 对象
-        int count = 0;
-
-        while(m.find()) {
-            System.out.println(m.groupCount() );
-            System.out.println("Found value: " + m.group(0) );
-            count++;
-            System.out.println("Match number "+count);
-            System.out.println("start(): "+m.start());
-            System.out.println("end(): "+m.end());
-        }
-    }
-}
-class RegexMatches2
-{
-    private static final String REGEX = "foo";
-    private static final String INPUT = "fooooooooooooooooo";
-    private static final String INPUT2 = "ooooofoooooooooooo";
-    private static Pattern pattern;
-    private static Matcher matcher;
-    private static Matcher matcher2;
-
-    public static void main( String args[] ){
-        pattern = Pattern.compile(REGEX);
-        matcher = pattern.matcher(INPUT);
-        matcher2 = pattern.matcher(INPUT2);
-
-        System.out.println("Current REGEX is: "+REGEX);
-        System.out.println("Current INPUT is: "+INPUT);
-        System.out.println("Current INPUT2 is: "+INPUT2);
-
-
-        System.out.println("lookingAt(): "+matcher.lookingAt());
-        System.out.println("matches(): "+matcher.matches());
-        System.out.println("lookingAt(): "+matcher2.lookingAt());
-    }
-}
-class RegexMatches3
-{
-    private static String REGEX = "dog";
-    private static String INPUT = "The dog says meow. " +
-            "All dogs say meow.";
-    private static String REPLACE = "cat";
-
-    public static void main(String[] args) {
-        Pattern p = Pattern.compile(REGEX);
-        // get a matcher object
-        Matcher m = p.matcher(INPUT);
-        INPUT = m.replaceAll(REPLACE);
-        System.out.println(INPUT);
-    }
-}
-class RegexMatches4
-{
-    private static String REGEX = "a*b";
-    private static String INPUT = "aabfooaabfooabfoobkkk";
-    private static String REPLACE = "-";
-    public static void main(String[] args) {
-        Pattern p = Pattern.compile(REGEX);
-        // 获取 matcher 对象
-        Matcher m = p.matcher(INPUT);
-        StringBuffer sb = new StringBuffer();
-        while(m.find()){
-            m.appendReplacement(sb,REPLACE);
-        }
-        m.appendTail(sb);
-        System.out.println(sb.toString());
-    }
 }
