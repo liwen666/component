b643e87518138dc315dbf768d2e96f2f4d1e945a
liwen <1438131288@qq.com>
2019-07-19 16:31:20 +0800

    redis基础测试

---
+++ redis/src/main/java/com/temp/component/redis/redis/BaseTest.java
@@ -0,0 +1,9 @@
+package com.temp.component.redis.redis;
+
+/**
+ * author lw
+ * date 2019/7/19  15:34
+ * discribe 基础方法测试
+ */
+public class BaseTest {
+}
---
+++ redis/src/main/java/com/temp/component/redis/redis/Main.java
@@ -0,0 +1,10 @@
+package com.temp.component.redis.redis;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        new RedisClient().show(); 
+    }
+
+}
