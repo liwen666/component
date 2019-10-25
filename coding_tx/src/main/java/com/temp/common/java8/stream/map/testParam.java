3466581f439273413ca4fc64db76bceacf73622f
liwen <534093268@qq.com>
2019-10-25 20:56:35 +0800

    java8

--- coding_tx/src/main/java/com/temp/common/java8/stream/map/testParam.java
+++ coding_tx/src/main/java/com/temp/common/java8/stream/map/testParam.java
@@ -1,10 +1,14 @@
 package com.temp.common.java8.stream.map;
 
 import com.alibaba.fastjson.JSON;
+import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public  class testParam {
     @org.junit.Test
@@ -28,4 +32,20 @@ public  class testParam {
     }
 
 
+    @Test
+    public void List() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        System.out.println(list);
+
+
+        List<String> collect = list.stream().map(string -> {
+            return "" + string;
+        }).collect(Collectors.toList());
+        System.out.println(collect);
+//        list.stream().collect(e->)
+
+
+    }
 }
